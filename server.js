c678172d77953245f27182ff8e157e4128d62f8e
j-paterson <jesse.r.pat@gmail.com>
2017-10-10 06:32:17 +0000

    server including /2016

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ var path = require('path');
 var app = express();
 
 // Define the port to run on
-app.set('port', 3000);
+app.set('port', 8080);
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/2016', function (req, res) {
+  res.sendFile(__dirname+'/public/2016.html')
+})
 
 // Listen for requests
 var server = app.listen(app.get('port'), function() {
